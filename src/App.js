2e16d778f3da56add1e0647b53b1cf13ad63726b
Bashua Mutiat <mutiatbashua@gmail.com>
2023-05-07 23:28:45 +0100

    question two - prop validation, installed prop-types

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ItemList from "./items";
-import StatusDiv from "./styleddiv"
+import Myprop from "./proptype";
+import StatusDiv from "./styleddiv";
 
 function App() {
-  
   return (
     <div className="App">
-<StatusDiv/>
+      {/* <StatusDiv/> */}
+      <Myprop name="Mutiat" age={22} email="mutiatbashua@gmail.com" />
     </div>
   );
 }
