7598d604c10ee7c0b7ff2f60c7b711f41bbc25ad
Bashua Mutiat <mutiatbashua@gmail.com>
2023-05-07 21:17:02 +0100

    fixing bug

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ItemList from "./items";
+import StatusDiv from "./styleddiv"
 
 function App() {
-  const items = [
-    { id: 1, name: "Book" },
-    { id: 2, name: "Plate" },
-    { id: 3, name: "Cup" },
-    { id: 4, name: "Apple" },
-    { id: 5, name: "Mango" },
-    { id: 6, name: "Strawberry" },
-    { id: 7, name: "Banana" },
-  ];
+  
   return (
     <div className="App">
-      <h2>List items</h2>
-      <ItemList items={items} />
+<StatusDiv/>
     </div>
   );
 }
---
+++ src/styleddiv.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+const StatusDiv = styled.div`
+  width: 100%;
+  background-color: ${(props) =>
+    props.status === "success"
+      ? "green"
+      : props.status === "warning"
+      ? "yellow"
+      : props.staus === "error"
+      ? "red"
+      : "white"};
+`;
+export default StatusDiv
